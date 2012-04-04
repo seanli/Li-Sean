ccdb68fde171b9d63199af018242f5ea924ac342
Elesant <lishang106@gmail.com>
2012-04-04 15:36:16 -0700

    Better blog main page.

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,14 @@ from creoleparser import text2html
 def blog(request, post_id=None):
     context = RequestContext(request)
     if post_id is None:
-        post_list = Post.objects.all()
-        context['posts'] = post_list
+        posts_data = []
+        posts = Post.objects.all().order_by('-created_time')[:5]
+        for post in posts:
+            post_datum = {}
+            post_datum['post'] = post
+            post_datum['parsed'] = text2html(post.content)
+            posts_data.append(post_datum)
+        context['posts_data'] = posts_data
         return render_to_response('blog/main.html', context)
     else:
         try:
